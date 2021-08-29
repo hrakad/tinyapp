820ffc0ab6ffad0b6fb6f2383e90c386f288128c
hrakad <your@email.com>
2021-08-29 22:52:49 +0000

    Add new route to render the urls_show.ejs template

--- express_server.js
+++ express_server.js
@@ -21,7 +21,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templatesVars = { urls: urlDatabase };
+  res.render("urls_index", templatesVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templatesVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templatesVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listen on port ${PORT}!`);
 });
-
