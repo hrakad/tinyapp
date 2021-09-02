ce5b748471f73fb1ce0d8076aeee9a984bc41430
hrakad <your@email.com>
2021-09-02 07:40:36 +0000

    Add route to remove a URL source

--- express_server.js
+++ express_server.js
@@ -50,6 +50,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listen on port ${PORT}!`);
 });
