e3001ff3dbd8ed7436ad7b8a9a24388941f021a0
hrakad <your@email.com>
2021-09-05 23:47:16 +0000

    Add the helpers function and use them as module

--- helper.js
+++ helper.js
@@ -9,4 +9,37 @@ function getUserByEmail(users, email) {
 }
 
 
-module.exports = { getUserByEmail }
+function getUserById(users, userId) {
+  let user = {};
+  for (const key of Object.keys(users)) {
+    if (key === userId) {
+      user = (users[key]);
+    }
+  }
+  return user;
+};
+
+function generateRandomString() {
+  let result = "";
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+};
+
+const getUrlsById = function(urlDatabase, userId) {
+  let object = {};
+
+  for (let url in urlDatabase) {
+    if (urlDatabase[url]['userID'] === userId) {
+      object[url] = {
+        longURL: urlDatabase[url]['longURL'],
+        userID: urlDatabase[url]['userID']
+      };
+    }
+  }
+  return object;
+};
+
+module.exports = { getUserByEmail, getUserById, generateRandomString, getUrlsById }
